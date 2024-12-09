f133d0acad43382f88133e860918318419365f58
wenzeng <wenzeng@xinchao.com>
2024-12-09 16:34:14 +0800

    fix

--- biz/src/main/java/com/example/biz/controller/DesignController.java
+++ biz/src/main/java/com/example/biz/controller/DesignController.java
@@ -22,6 +22,6 @@ public class DesignController {
     @GetMapping("/chain")
     public void testChain() {
         CscQuery query = CscQuery.INSTANCE;
-        query.addNextQueryState(CsmQuery.INSTANCE);
+        query.addNextQueryState();
     }
 }
